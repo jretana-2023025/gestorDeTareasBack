fdd3830b0d12586f097c781777ea278966785dd6
JRetana-2023025 <jretana-2023025@kinal.edu.gt>
2025-05-14 23:31:23 -0600

    Create post.model.js

---
+++ src/post/post.model.js
@@ -0,0 +1,32 @@
+import { Schema,model } from "mongoose";
+
+const postSchema = new Schema(
+    {
+        title:{
+            type:String,
+            required:[true,'Title is required'],
+            maxLength:[100,'Title is too long']
+        },
+        content:{
+            type:String,
+            required:[true,'Content is required'],
+            maxLength:[500,'Content is too long']
+        },
+        course:{
+            type:String,
+            required:[true,'Course is required'],
+            maxLength:[50,'Course is too long']
+        },
+        date:{
+            type:Date,
+            default:Date.now
+        },
+        comments:{
+            type:Schema.Types.ObjectId,
+            ref:'Comment',
+            
+        }
+    }
+)
+
+export default  model('Post',postSchema)
